0753dca2ea742c0875fd83b8faeeb44c0d0175b8
Ben Todd <btodd@multiservice.com>
2019-01-18 16:11:01 -0600

    Added post method

--- server.js
+++ server.js
@@ -2,10 +2,22 @@ var express = require('express');
 var app = express();
 var fs = require("fs");
 
-app.get('/listUsers', function (req, res) {
+var user = {
+   "user4" : {
+      "name" : "mohit",
+      "password" : "password4",
+      "profession" : "teacher",
+      "id": 4
+   }
+}
+
+app.post('/addUser', function (req, res) {
+   // First read existing users.
    fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+      data = JSON.parse( data );
+      data["user4"] = user["user4"];
       console.log( data );
-      res.end( data );
+      res.end( JSON.stringify(data));
    });
 })
 
